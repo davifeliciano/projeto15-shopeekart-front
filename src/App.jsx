5fd4313efd777c77702c3684eab88faf78fb87ba
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-27 20:42:50 -0300

    Register route add on App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Root from "./routes/Root";
+import Register from "./routes/Register";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Root />} />
+      <Route path="/register" element={<Register />} />
     </Routes>
   );
 }
