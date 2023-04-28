2e675e178e48720148c522de7e141a49a295df55
Thomas Andrioli Leick <thomasaleick@gmail.com>
2023-04-28 17:38:52 -0300

    PersistLogin ok, scoope of the loadingPage

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Root from "./routes/Root";
 import SignUp from "./routes/SignUp";
 import Login from "./routes/Login";
+import PersistLogin from "./components/PersistLogin";
 
 axios.defaults.baseURL = import.meta.env.VITE_APP_API_URL;
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Root />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/login" element={<Login />} />
+      <Route element={<PersistLogin />}>
+        <Route path="/" element={<Root />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<Login />} />
+      </Route>
     </Routes>
   );
 }
---
+++ src/components/PersistLogin.jsx
@@ -0,0 +1,34 @@
+import { Outlet } from "react-router-dom";
+import { useState, useEffect } from "react";
+import useRefreshToken from "../hooks/useRefreshToken";
+import useAuth from "../hooks/useAuth";
+import React from "react";
+import LoadingPage from "./LoadingPage";
+
+const PersistLogin = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const refresh = useRefreshToken();
+  const { auth } = useAuth();
+    console.log(auth)
+  useEffect(() => {
+    let isMounted = true;
+    const verifyRefreshToken = async () => {
+      try {
+        await refresh();
+      } catch (err) {
+        console.log(err);
+      } finally {
+        isMounted && setIsLoading(false);
+      }
+    };
+
+    !auth?.accessToken ? verifyRefreshToken() : setIsLoading(false);
+
+    return () => (isMounted = false);
+  }, []);
+
+  return (
+    <>{isLoading ? <LoadingPage /> : <Outlet />}</>
+  );
+};
+export default PersistLogin;
